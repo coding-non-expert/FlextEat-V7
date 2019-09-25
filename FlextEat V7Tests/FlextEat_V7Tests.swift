52902d766a726ae22e23062e8adac3da43965d5d
celeste <celiresmart@gmail.com>
2019-09-25 20:16:56 +0800

    Initial Commit

---
+++ FlextEat V7Tests/FlextEat_V7Tests.swift
@@ -0,0 +1,34 @@
+//
+//  FlextEat_V7Tests.swift
+//  FlextEat V7Tests
+//
+//  Created by Celeste Tan on 25/9/19.
+//  Copyright © 2019 cuzwhynot. All rights reserved.
+//
+
+import XCTest
+@testable import FlextEat_V7
+
+class FlextEat_V7Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
